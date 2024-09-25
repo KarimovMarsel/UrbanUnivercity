0e30cf3968f5f1c6b9f67db94519ccc7fe4bbf24
KarimovMarsel <karimovm02@mail.ru>
2024-09-25 11:42:41 +0500

    Add files via upload

---
+++ 2)Способы вызова функции.py
@@ -0,0 +1,26 @@
+def checker(email):
+    domains_for_check = ('com', 'ru', 'net')
+    if email == 'university.help@gmail.com':
+        return True
+    elif (email.split('.')[-1] in domains_for_check) and ('@' in email):
+        return True
+    return False
+
+
+def send_email (message, recipient, sender = 'university.help@gmail.com'):
+    if sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+        return
+    if not (checker(sender) and checker(recipient)):
+        print('Невозможно отправить письмо с адреса <sender> на адрес <recipient>')
+        return
+    if sender == 'university.help@gmail.com':
+        print('Письмо успешно отправлено с адреса <sender> на адрес <recipient>')
+    else:
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <sender> на адрес <recipient>.')
+        return
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
